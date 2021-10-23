22701e2d342bb3db30e5d624fc03d5243d1c82d3
kampersanda <shnsk.knd@gmail.com>
2021-10-24 01:24:58 +0900

    minor

--- sample/sample_rlbwt.cpp
+++ sample/sample_rlbwt.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
     // Input text
     const std::string text = "abaababaab";
 
-    // Construct the r-index by appending characters (with end-marker $) in reverse.
+    // Construct the RLBWT by appending characters (with end-marker $) in reverse.
     // Note that '\0' is used for the end marker (i.e., the text should not contain '\0').
     rlbwt_types::glfig_serialized<8>::type rlbwt;
     for (size_t i = 1; i <= text.size(); i++) {
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     rlbwt.output_runs([](const run_type& r) { std::cout << r << ","; });
     std::cout << std::endl;
 
-    // Decode the original text (except $) from the index
+    // Decode the original text (except $) from the RLBWT
     std::string decoded;
     rlbwt.decode_text([&](uchar_type c) { decoded.push_back(c); });
     std::reverse(decoded.begin(), decoded.end());  // need to be reversed
